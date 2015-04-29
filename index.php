5ec939da0f5b0b0254b31787d63d12920065fed2
Henry Ramirez <h.ramirez0427@gmail.com>
2015-04-29 09:05:01 -0700

    commit 2

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html>
 <head>
 	<title></title>
+	<link rel="stylesheet" type="text/css" href="js/main.js">
 </head>
 <body>
 <div class="dan">
@@ -9,5 +10,6 @@
 <div class="iwao">
 	<button type="button">Click Me!</button>
 </div>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 </body>
 </html>
